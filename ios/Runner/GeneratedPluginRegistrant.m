28c339d7ab4b4eb477073ed925969c13ca4b5612
liqingyong <735314662@qq.com>
2022-12-13 18:45:00 +0800

    提交信息

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<fijkplayer/FijkPlugin.h>)
+#import <fijkplayer/FijkPlugin.h>
+#else
+@import fijkplayer;
+#endif
+
+#if __has_include(<wakelock/WakelockPlugin.h>)
+#import <wakelock/WakelockPlugin.h>
+#else
+@import wakelock;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FijkPlugin registerWithRegistrar:[registry registrarForPlugin:@"FijkPlugin"]];
+  [WakelockPlugin registerWithRegistrar:[registry registrarForPlugin:@"WakelockPlugin"]];
+}
+
+@end
